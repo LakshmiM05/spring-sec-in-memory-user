0f801304b44f21b481139283e9c6b8769c19f7ab
Lakshmi <lakshmigid@gmail.com>
2024-06-16 11:12:22 +0530

    First commit

---
+++ src/main/java/com/example/sec/controller/ContentController.java
@@ -0,0 +1,23 @@
+package com.example.sec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContentController {
+	
+	@GetMapping("/home")
+	public String getHomeContent() {
+		return "home";
+	}
+	
+	@GetMapping("/admin/home")
+	public String getAdminHomeContent() {
+		return "admin-home";
+	}
+	
+	@GetMapping("/user/home")
+	public String getUserHomeContent() {
+		return "user-home";
+	}
+}
---
+++ src/test/java/com/example/sec/in_memory_user/InMemoryUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sec.in_memory_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemoryUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
